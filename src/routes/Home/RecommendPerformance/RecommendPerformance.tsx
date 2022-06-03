23491bb4d7a4d0a73128350d1b365c9a11b84a83
유재민 <yoojamer@gmail.com>
2022-06-04 03:00:26 +0900

    home 컴포넌트 수정

--- src/routes/Home/RecommendPerformance/RecommendPerformance.tsx
+++ src/routes/Home/RecommendPerformance/RecommendPerformance.tsx
@@ -1,19 +1,17 @@
-import { useNavigate } from 'react-router-dom';
 import { useQuery } from 'react-query';
 
+import Spinner from 'components/Spinner/Spinner';
+import ItemList from 'components/ItemList/ItemList';
+
+import useApiDataType from 'hooks/useApiDataType';
 import { getPerformanceListApi } from 'services/api';
 
 import styles from './recommendPerformance.module.scss';
 
-import useApiDataType from 'hooks/useApiDataType';
-
-import Spinner from 'components/Spinner/Spinner';
-
 const RecommendPerformance = () => {
-  const navigate = useNavigate();
   const { handleXmlChange } = useApiDataType();
 
-  const { data, isLoading, isError } = useQuery(
+  const { data, isLoading } = useQuery(
     ['getPerformanceListApi'],
     () =>
       getPerformanceListApi({ cpage: '1', rows: '5' })
@@ -26,37 +24,10 @@ const RecommendPerformance = () => {
     }
   );
 
-  const goToDetail = (mt20id: string) => {
-    navigate(`/${mt20id}`);
-  };
-
-  const dataCheck = data ?? [];
-
   return (
     <div className={styles.performanceWrap}>
       <h1 className={styles.title}>추천 공연</h1>
-      <ul className={styles.itemListWrap}>
-        {dataCheck.map((item: any) => {
-          const { mt20id, fcltynm, poster, genrenm, prfnm, prfpdfrom, prfpdto } = item;
-          const key = `${prfnm}+${fcltynm}`;
-
-          return (
-            <li key={key} className={styles.itemList}>
-              <button type='button' onClick={() => goToDetail(mt20id)} className={styles.detailBtn}>
-                <img className={styles.poster} src={poster} alt={fcltynm} />
-                <div className={styles.descWrap}>
-                  <p className={styles.itemTitle}>{prfnm}</p>
-                  <p className={styles.period}>
-                    {prfpdfrom} ~ {prfpdto}
-                  </p>
-                  <p className={styles.host}>{fcltynm}</p>
-                </div>
-                <span className={styles.genrenm}>{genrenm}</span>
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      <ItemList itemArray={data ?? []} />
       {isLoading && <Spinner top={220} bottom={220} />}
     </div>
   );
