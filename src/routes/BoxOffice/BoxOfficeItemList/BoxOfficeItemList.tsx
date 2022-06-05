68a36aa8ed4779e42fa310fc70c922d3a5dbbf2d
Yu jae min <85284246+Yu-jae-min@users.noreply.github.com>
2022-06-05 15:39:05 +0900

    예매현황 이미지 url 데이터 수정 (#4)

--- src/routes/BoxOffice/BoxOfficeItemList/BoxOfficeItemList.tsx
+++ src/routes/BoxOffice/BoxOfficeItemList/BoxOfficeItemList.tsx
@@ -4,6 +4,8 @@ import { IBoxOfficeListType } from 'types/types';
 
 import styles from './boxOfficeItemList.module.scss';
 
+import { BOXOFFICE_BASE_URL } from 'models/models';
+
 interface IitemArrayType {
   itemArray: IBoxOfficeListType[];
 }
@@ -21,12 +23,13 @@ const BoxOfficeItemList = ({ itemArray }: IitemArrayType) => {
       {itemArray.map((item: IBoxOfficeListType) => {
         const { prfplcnm, prfnm, rnum, poster, mt20id } = item;
         const key = `${prfnm}+${rnum}`;
+        const posterUrl = `${BOXOFFICE_BASE_URL}${poster}`;
 
         return (
           <li key={key} className={styles.itemList}>
             <button type='button' onClick={() => goToDetail(mt20id)} className={styles.detailBtn}>
               <div className={styles.imgWrap}>
-                <img className={styles.poster} src={poster} alt={prfplcnm} />
+                <img className={styles.poster} src={posterUrl} alt={prfplcnm} />
               </div>
               <span className={styles.rnum}>{rnum}</span>
             </button>
