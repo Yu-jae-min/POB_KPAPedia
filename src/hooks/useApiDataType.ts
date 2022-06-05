93c2ced706b288c4a88a6d3449b260eab682cac3
유재민 <yoojamer@gmail.com>
2022-06-05 14:11:45 +0900

    api

--- src/hooks/useApiDataType.ts
+++ src/hooks/useApiDataType.ts
@@ -7,9 +7,6 @@ interface IXmlChangeType {
 export const boxOfficeXmlChange = (xml: string) => {
   let newData;
 
-  // eslint-disable-next-line no-console
-  console.log('xml', xml);
-
   new xml2js.Parser().parseString(xml, (err, result) => {
     const changeValue = result.boxofs.boxof.map((item: IXmlChangeType) => {
       const keys = Object.keys(item);
