853303ad8c5e6ea5aac001d3eab5055baa168752
유재민 <yoojamer@gmail.com>
2022-06-26 22:24:59 +0900

    컴포넌트 레이지 로딩 기능 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,31 @@
+import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Layout from './_shared/Layout/Layout';
-import Login from './Login/Login';
-import Home from './Home/Home';
-import Performance from './Performance/Performance';
-import Festival from './Festival/Festival';
-import Award from './Awards/Awards';
-import BoxOffice from './BoxOffice/BoxOffice';
-import Detail from '../components/Detail/Detail';
+const Home = lazy(() => import('./Home/Home'));
+const Layout = lazy(() => import('./_shared/Layout/Layout'));
+const Login = lazy(() => import('./Login/Login'));
+const Performance = lazy(() => import('./Performance/Performance'));
+const Festival = lazy(() => import('./Festival/Festival'));
+const Award = lazy(() => import('./Awards/Awards'));
+const BoxOffice = lazy(() => import('./BoxOffice/BoxOffice'));
+const Detail = lazy(() => import('../components/Detail/Detail'));
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='login' element={<Login />} />
-      <Route element={<Layout />}>
-        <Route path='/' element={<Home />} />
-        <Route path='boxOffice' element={<BoxOffice />} />
-        <Route path='performance' element={<Performance />} />
-        <Route path='festival' element={<Festival />} />
-        <Route path='award' element={<Award />} />
-        <Route path='/:detail' element={<Detail />} />
-      </Route>
-      <Route path='*' element={<div>잘못 된 접근입니다.</div>} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path='login' element={<Login />} />
+        <Route element={<Layout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='boxOffice' element={<BoxOffice />} />
+          <Route path='performance' element={<Performance />} />
+          <Route path='festival' element={<Festival />} />
+          <Route path='award' element={<Award />} />
+          <Route path='/:detail' element={<Detail />} />
+        </Route>
+        <Route path='*' element={<div>잘못 된 접근입니다.</div>} />
+      </Routes>
+    </Suspense>
   );
 };
 
